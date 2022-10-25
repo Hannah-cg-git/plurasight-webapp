51b2a439c2f3cc22cb2839a1d31308214ef2baf4
Hannah-cg-git <116548945+Hannah-cg-git@users.noreply.github.com>
2022-10-25 12:50:26 +1100

    change 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!Excited with the 1st publish");
 
 app.Run();
